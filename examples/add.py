cc05b43d827f80f01b7d3aa7b1bb6ba7d14120e3
AudreyFitchett <audreyfitchett@u.boisestate.edu>
2026-04-19 16:29:14 -0600

    In progress - adding examples for presentation

--- examples/add.py
+++ examples/add.py
@@ -5,3 +5,9 @@ def add_numbers(a, b):
 result = add_numbers(10, 5)
 print(f"The sum is: {result}")
 
+# shows off the syntax for one number to the power of another number
+# can also use this to show how _ works, but will need to copy it in terminal 
+def power(a):
+        return 2 ** a
+
+# imaginary numbers
---
+++ examples/strings.py
@@ -0,0 +1,7 @@
+
+# repeating strings 
+def summons():
+    a = "beetlejuice"
+    print(2 * a)
+
+summons()
